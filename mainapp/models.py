75bb97dbf33d3de43edba3db142059c751d4e24f
recipy <rajeshvishwakarma7280@gmail.com>
2023-10-16 21:22:02 +0530

    add filter code

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,7 +5,10 @@ class reciepe_model(models.Model):
     image = models.ImageField()
     price = models.IntegerField(null=True,blank=True)
     description = models.TextField(null=True,blank=True)
-    
+    class Meta:
+        ordering = ['name'] # iske use se alfabetical(a-z) order me name ka data store hoga
+        # ordering = ['name'] # iske use se alfabetical(z-a) order me name ka data save hoga
+        # verbose_name = "reciepemodel" # iske use se reciepe_model ka name reciepemodel ho jaega
     
 #hit count model
 class URLHitCount(models.Model):
--- mainapp/views.py
+++ mainapp/views.py
@@ -43,7 +43,7 @@ def loginView(request):
         if user_check is None:
             Warning = "Username and password not mach"
             return render(request,"login.html",{"warning":Warning})
-        #session manage agar sassion valid hai to use automatic login ho jaega
+        #session manage/agar sassion valid hai to user automatic login ho jaega
         login(request,user_check)
         return redirect("form")
     return render(request,"login.html")
@@ -54,18 +54,34 @@ def logoutView(request):
     logout(request)
     return redirect("/login/")
     
-
+# aggregation ke lie import module
+from django.db.models import Count,Avg,Sum,Min,Max
 #get
 @login_required(login_url="/login/")
 def formView(request):
     if request.method.lower() == "get":
+        #aggregation
+        count_find = reciepe_model.objects.aggregate(Count('price'))
+        avarage_find = reciepe_model.objects.aggregate(Avg('price'))
+        sum_find = reciepe_model.objects.aggregate(Sum('price'))
+        min_find = reciepe_model.objects.aggregate(Min('price'))
+        max_find = reciepe_model.objects.aggregate(Max('price'))
+        print("count_find:",count_find['price__count'],"sum_find:",sum_find['price__sum'],"avarage_find:",avarage_find['price__avg'],"min_find:",min_find['price__min'],"max_find:",max_find['price__max'],"aaaaaaaaaaaaaaaaaaaa")
+        # datafilter = reciepe_model.objects.filter(price=min_find["price__min"]) 
+        # print(datafilter)
+        
+        #annotation
+        annotatedata = reciepe_model.objects.values('price').annotate(Min('id'))
+        print(annotatedata,"aaaaaaaaaaaaaaaaaaaa")      
+        
         #hit count
         current_url = request.path_info
         url_hit_count, created = URLHitCount.objects.get_or_create(url=current_url)
         url_hit_count.hit_count += 1
         url_hit_count.save()
-        #------------        
-        return render(request, "index.html")
+        #------------   
+        data = reciepe_model.objects.all().order_by('name')
+        return render(request, "index.html",{"data":data})
 
 #post
 @login_required(login_url="/login/")
@@ -93,15 +109,15 @@ def pricerenageView(request):
     min_value = request.POST['min_value'] if request.POST['min_value'] !="" else 0
     max_value = request.POST['max_value'] if request.POST['max_value'] !="" else 0
     if min_value !=0 and max_value !=0:
-        range_value = reciepe_model.objects.filter(Q(price__gte = min_value) & Q(price__lte = max_value))
+        range_value = reciepe_model.objects.filter(Q(price__gte = min_value) & Q(price__lte = max_value)).order_by("-id")
     
     elif min_value !=0:
-        range_value = reciepe_model.objects.filter(Q(price__gte = min_value))
+        range_value = reciepe_model.objects.filter(Q(price__gte = min_value)).order_by("-id")
     
     elif max_value !=0:
-        range_value = reciepe_model.objects.filter(Q(price__lte = max_value))
+        range_value = reciepe_model.objects.filter(Q(price__lte = max_value)).order_by("-id")
     else:
-        range_value = reciepe_model.objects.all()
+        range_value = reciepe_model.objects.all().order_by("-id")
 
     return render(request,"index.html",{"data":range_value})
 
